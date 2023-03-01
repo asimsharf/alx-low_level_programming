75860ab51e205b962d292c8b372f0c8e9cb1774d
asimsharf <asimsharf@gmail.com>
2023-03-01 08:32:23 -0800

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,18 +14,13 @@
  *         respectively, to be less than, to match, or be greater than s2.
  */
 
-char *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
-int i = 0;
-
-while (s1[i] != '\0' && s2[i] != '\0')
+while (*s1 && *s2 && *s1 == *s2)
 {
-if (s1[i] != s2[i])
-{
-return (s1[i] - s2[i]);
-}
-i++;
+s1++;
+s2++;
 }
 
-return (0);
+return (*s1 - *s2);
 }
