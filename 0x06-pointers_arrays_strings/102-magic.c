82e784693d7037c41758c05079deb654bc26e4f4
asimsharf <asimsharf@gmail.com>
2023-03-02 13:33:26 -0800

    update

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,14 @@
+/*
+* File: 101-magic.c
+* Auth: Asim Sharfeldin
+*/
+
 #include <stdio.h>
+
+/*
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
@@ -16,7 +26,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  p[5] = 98;
+  *(p + 5) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
