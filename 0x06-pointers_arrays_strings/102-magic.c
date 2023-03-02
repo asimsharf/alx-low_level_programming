980c2a9f907f492493660bd545e9671057cb5710
asimsharf <asimsharf@gmail.com>
2023-03-02 09:24:51 -0800

    update

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -14,7 +14,6 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-p[5] = 98;
 /*
 * write your line of code here...
 * Remember:
@@ -23,7 +22,7 @@ p[5] = 98;
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-
+p[5] = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
