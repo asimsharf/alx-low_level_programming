83cabe4a5b0777e5c5e363f748267921ec430845
asimsharf <asimsharf@gmail.com>
2023-03-06 14:00:21 -0800

    updated

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,12 +7,13 @@
  * Description: fills the first n bytes of the memory area pointed to by s
  */
 
-char *_memset(char *s, char b, unsigned int n)
-{
+char *_memset(char *s, char b, unsigned int n){
   unsigned int i;
-  for (i = 0; i < n; i++)
-    {
-      s[i] = b;
-    }
-  return s;
+
+  for (i = 0; i < n; i++){
+    s[i] = b;
+  }
+
+  return (s);
+
 }
