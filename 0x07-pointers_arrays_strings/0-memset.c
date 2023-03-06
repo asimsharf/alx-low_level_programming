79ef909ec6c8f5a570d69e8515bd81fbd09db24f
asimsharf <asimsharf@gmail.com>
2023-03-06 14:18:21 -0800

    update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,9 +10,10 @@
 char *_memset(char *s, char b, unsigned int n){
   unsigned int i;
 
-  for (i = 0; i < n; i++){
-    s[i] = b;
-  }
+  unsigned char *memory = s, v = c;
 
-  return (s);
+  for(i = 0; i < n; i++)
+    memory[i] = v;
+
+  return (memory);
 }
