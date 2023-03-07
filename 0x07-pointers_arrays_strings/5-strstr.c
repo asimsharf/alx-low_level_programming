3f57034b4681a811bb396bc266642f16ed0ad181
asimsharf <asimsharf@gmail.com>
2023-03-06 23:26:59 -0800

    update

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,18 +6,19 @@
  * substring is not found
  * Description: finds the first occurrence of the substring needle in the
 */
+
 char *_strstr(char *haystack, char *needle)
 {
-int i, j;
-for (i = 0; haystack[i] != '\0'; i++)
-{
-for (j = 0; needle[j] != '\0'; j++)
-{
-if (haystack[i] == needle[j])
-{
-return (haystack + i);
-}
-}
-}
-return ('\0');
+	int i, j, k;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = i, k = 0; needle[k] != '\0' && haystack[j] == needle[k]; j++, k++)
+			;
+		if (needle[k] == '\0')
+		{
+			return (haystack + i);
+		}
+	}
+	return ('\0');
 }
