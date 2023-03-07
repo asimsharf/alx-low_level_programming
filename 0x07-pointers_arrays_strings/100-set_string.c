50513ba509f4cf1ac0781f3228d01e1285e30063
asimsharf <asimsharf@gmail.com>
2023-03-06 23:44:49 -0800

    update

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to a pointer
+ * @to: pointer to a char
+ * Return: void
+ * Description: function that sets the value of a pointer to a char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
