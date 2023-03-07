6089b5357d253943978201a9552ea1cdd5230185
asimsharf <asimsharf@gmail.com>
2023-03-06 22:59:04 -0800

    update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,14 +8,11 @@
 
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
-		{
-			return (s + i);
-		}
-	}
-	return (s = 0);
+  while(*s != '\0'){
+    if(*s == c){
+      return (s);
+    }
+    i++;
+  }
+  return (s = 0);
 }
