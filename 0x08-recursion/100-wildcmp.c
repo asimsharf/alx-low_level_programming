53c9946f24560b5182c9f113685bb15ea48fecb2
asimsharf <asimsharf@gmail.com>
2023-03-08 00:06:08 -0800

    update

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,10 +1,5 @@
 #include "main.h"
 
-int strlen_no_wilds(char *str);
-void iterate_wild(char **wildstr);
-char *postfix_match(char *str, char *postfix);
-int wildcmp(char *s1, char *s2);
-
 /**
  * strlen_no_wilds - Returns the length of a string,
  *                   ignoring wildcard characters.
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,8 +9,14 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
 
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
+int strlen_no_wilds(char *str);
+void iterate_wild(char **wildstr);
+char *postfix_match(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
 
 #endif
