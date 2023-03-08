5e6806d923cedbeca4ba07a85427e90f3b72f072
asimsharf <asimsharf@gmail.com>
2023-03-08 00:22:21 -0800

    update

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -3,12 +3,12 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @character: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int _putchar(char character)
 {
-	return (write(1, &c, 1));
+	return (write(1, &character, 1));
 }
