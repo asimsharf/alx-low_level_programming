9d294f8bd7f3632a1d46377157e9422e54db24ed
asimsharf <asimsharf@gmail.com>
2023-03-13 21:27:03 -0700

    update

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - concatenate two numbers
+ * @s1: char 1
+ * @s2: char 2
+ * Return: char concatenate
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *res;
+	int i = 0, j = 0, cont, k = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i])
+	{
+		i++;
+	}
+
+	while (s2[j])
+	{
+		j++;
+	}
+
+	k = i + j + 1;
+
+	res = malloc(k * sizeof(char));
+
+	if (res == NULL)
+		return (NULL);
+
+	for (cont = 0; cont < i; cont++)
+	{
+		res[cont] = s1[cont];
+	}
+
+	for (cont = 0; cont <= j; cont++)
+	{
+		res[cont + i] = s2[cont];
+	}
+
+	return (res);
+}
