8020a61cb122ff204aa47432d7cadc56a082bb27
asimsharf <asimsharf@gmail.com>
2023-03-31 06:57:26 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,13 @@
 section .text
         global _start
 _start:
-    mov edx, len                ;length of message
-    mov ecx, msg                ;message to print
-    mov ebx, 1                  ;file descriptor
-    mov eax, 4                  ;system call
-    int 0x80                    ;to call kernel
+    mov edx, len
+    mov ecx, msg
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
 
 section .data
-msg db "Hello, Holberton",0xa,  ;message to print 0xa is for new line
-len equ $ - msg                 ;length of message
-
-section .bss
+	msg db "Hello, Holberton",0xa,
+	len equ $ - msg
+	
