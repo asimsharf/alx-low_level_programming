544770db19ef4b8a79cdc5ff61c7695c923cff4c
asimsharf <asimsharf@gmail.com>
2023-03-31 03:27:44 -0700

    Write a function that prints You're beat! and yet, you must allow,\nI bore my house upon my backbefore the main function is executed

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+ * first - a function that prints before the main function is executed.
+ * Return: void
+*/
+
+void __attribute__ ((constructor)) premain(void) {
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
 int main(void)
 {
 	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
