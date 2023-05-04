bf6ecbefaed4679c4d57fb998d9e0e390e9e4c82
asimsharf <asimsharf@gmail.com>
2023-05-05 00:50:30 +0300

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,21 +8,8 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int i = 1;
-	int count = 0;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	while (i <= n)
-	{
-		i <<= 1;
-		count++;
-	}
-	while (count > 0)
-	{
-		i >>= 1;
-		if (n & i)
-			_putchar('1');
-		else
-			_putchar('0');
-		count--;
-	}
+	_putchar((n & 1) + '0');
 }
