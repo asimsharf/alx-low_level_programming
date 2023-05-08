c5a5bfe1ba073ba4d4cae87e1c1f87d13785a217
asimsharf <asimsharf@gmail.com>
2023-05-08 13:36:51 +0300

    read text file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+
+	int fd, written, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		written = write(fd, text_content, i);
+		if (written == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
