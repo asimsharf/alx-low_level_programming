d40036dbe8139c589de82d6cf6a700d326f0a924
asimsharf <asimsharf@gmail.com>
2023-05-08 14:17:53 +0300

    betty formating lines

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -16,24 +16,26 @@ int main(int argc, char *argv[])
 {
 	int fd, readed;
 	char buffer[5];
+	char err0[] = "Usage: elf_header elf_filename\n";
+	char err1[] = "Error: Can't read from file ";
+	char err2[] = "Error: Not an ELF file ";
+	char err3[] = "Error: Can't close fd ";
 
 	if (argc != 2)
-		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n"), exit(98);
-
+		dprintf(STDERR_FILENO, "%s", err0), exit(98);
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
+		dprintf(STDERR_FILENO, "%s%s\n", err1, argv[1]), exit(98);
 	readed = read(fd, buffer, 4);
 	if (readed == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
+		dprintf(STDERR_FILENO, "%s%s\n", err1, argv[1]), exit(98);
 	buffer[4] = '\0';
-
-	if (buffer[0] == 0x7f && buffer[1] == 'E' && buffer[2] == 'L' && buffer[3] == 'F')
+	if (buffer[0] == 0x7f &&
+		buffer[1] == 'E' &&
+		buffer[2] == 'L' &&
+		buffer[3] == 'F')
 		printf("ELF Header:\n");
 	else
-		dprintf(STDERR_FILENO, "Error: Not an ELF file %s\n", argv[1]), exit(98);
-
+		dprintf(STDERR_FILENO, "%s%s\n", err2, argv[1]), exit(98);
 	return (0);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,39 +15,37 @@ int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, readed, closed, written;
 	char buffer[1024];
+	char err0[] = "Usage: cp file_from file_to\n";
+	char err1[] = "Error: Can't read from file ";
+	char err2[] = "Error: Can't write to ";
+	char err3[] = "Error: Can't close fd ";
+	char err4[] = "Error: Can't close fd ";
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-
+		dprintf(STDERR_FILENO, "%s", err0), exit(97);
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
+		dprintf(STDERR_FILENO, "%s%s\n", err1, argv[1]), exit(98);
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-
+		dprintf(STDERR_FILENO, "%s%s\n", err2, argv[2]), exit(99);
 	readed = read(fd_from, buffer, 1024);
 	if (readed == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
+		dprintf(STDERR_FILENO, "%s%s\n", err1, argv[1]), exit(98);
 	while (readed > 0)
 	{
 		written = write(fd_to, buffer, readed);
 		if (written == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO, "%s%s\n", err2, argv[2]), exit(99);
 		readed = read(fd_from, buffer, 1024);
 		if (readed == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+			dprintf(STDERR_FILENO, "%s%s\n", err1, argv[1]), exit(98);
 	}
-
 	closed = close(fd_from);
 	if (closed == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
-
+		dprintf(STDERR_FILENO, "%s%d\n", err3, fd_from), exit(100);
 	closed = close(fd_to);
 	if (closed == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
-
+		dprintf(STDERR_FILENO, "%s%d\n", err4, fd_to), exit(100);
 	return (0);
 }
