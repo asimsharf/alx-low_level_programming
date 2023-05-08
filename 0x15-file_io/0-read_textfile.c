863411e5a79ecdd097c1a31d5aa2b54d69420235
asimsharf <asimsharf@gmail.com>
2023-05-08 14:26:31 +0300

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,30 +9,22 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t readed, printed;
+	int fd, readed;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
-
 	readed = read(fd, buffer, letters);
 	if (readed == -1)
 		return (0);
-
-	printed = write(STDOUT_FILENO, buffer, readed);
-	if (printed == -1)
-		return (0);
-
-	free(buffer);
+	write(STDOUT_FILENO, buffer, readed);
 	close(fd);
-	return (printed);
+	free(buffer);
+	return (readed);
 }
