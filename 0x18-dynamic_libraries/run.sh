44fa56f455dce796adff0ca50285cc928de1277c
asimsharf <asimsharf@gmail.com>
2023-06-27 03:31:58 -0700

    python3 100-tests.py

--- 0x18-dynamic_libraries/run.sh
+++ 0x18-dynamic_libraries/run.sh
@@ -2,6 +2,7 @@
 gcc -Wall -pedantic -Werror -Wextra -c -fPIC *.c
 gcc -Wall -pedantic -Werror -Wextra -shared -o libdynamic.so *.o
 gcc -shared -o liball.so *.o
+gcc -shared -o 100-operations.so *.o
 # gcc -Wall -pedantic -Werror -Wextra *.c -o libdynamic -L/path/to/library -libdynamic
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 # ls -la lib*
