abb4b8085a93b5e9872f2f564936c1664723efd8
asimsharf <asimsharf@gmail.com>
2023-06-27 03:46:12 -0700

    update win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/asimsharf/alx-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
+wget -O /tmp/win.so https://github.com/asimsharf/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/win.so
